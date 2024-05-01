02bad8e390c7260d9d7ac2b054860dc255711c3b
michelebswm <michele.fenix.17@gmail.com>
2024-05-01 00:00:23 -0300

    feat: adiciona banco de dados models

--- controlepagamentos/models.py
+++ controlepagamentos/models.py
@@ -7,17 +7,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
+    senha = db.Column(db.String, nullable=False)
+    token = db.Column(db.String(50))
 
 
 class Pagamento(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(30), nullable=False)
     valor = db.Column(db.Numeric(10, 2), nullable=False)
     forma_pagamento = db.Column(db.String(30), nullable=False)
     parcelas = db.Column(db.Integer, nullable=False)
-    movimentacao = db.Column(db.DateTime)
+    data_webhook = db.Column(db.DateTime, nullable=False)
     acesso_curso = db.Column(db.Boolean, default=False)
     mensagem_enviada = db.Column(db.Boolean, default=False)
 
