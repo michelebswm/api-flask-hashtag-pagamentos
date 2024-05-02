8e156dd4b46dcfdbf934941e91b2c59a98b420a1
michelebswm <michele.fenix.17@gmail.com>
2024-05-02 19:33:28 -0300

    feat: adiciona run.py

--- controlepagamentos/__init__.py
+++ controlepagamentos/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from extensions import db, csrf, bcrypt, migrate, login_manager
+from controlepagamentos import models, views
 
 app = Flask(__name__)
 app.config.from_object("config")
@@ -13,8 +14,6 @@ login_manager.init_app(app=app)
 login_manager.login_view = 'login'   # Redireciona para a página lgoin que é a função definida no routes.py
 login_manager.login_message = "Você precisa estar logado para acessar esta página."
 login_manager.login_message_category = 'alert-info'
-
-from controlepagamentos import models, views
 
 
 if __name__ == '__main__':
